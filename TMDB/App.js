01adb263bffc2ba3b97753dcc66c313bc6e9f108
Novesiuee <aurorsenior@gmail.com>
2022-09-27 18:19:01 +0700

    feat: setup-react-native done

---
+++ TMDB/App.js
@@ -0,0 +1,31 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./src/screens/Home";
+export default function App() {
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerShown: false,
+          }}
+        />
+        {/* <Stack.Screen name="Detail" component={MovieDetail} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ TMDB/src/screens/Home.js
@@ -0,0 +1,19 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  StatusBar,
+  Dimensions,
+  FlatList,
+} from "react-native";
+export default function Home({ navigation }) {
+  return;
+}
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+  },
+});
